3ca76c7dd5ab82f5e7c3d5812e48d616209c398f
d4nse <mordorsocial@disroot.org>
2023-10-13 01:47:44 +0500

    minor typo changes, added wakelocks to zlogs

--- README.md
+++ README.md
@@ -11,8 +11,9 @@ this is termux deployment script written by d4nse
 
 
 # TODO
-- [x] Code termux config deployment
+- [x] Code termux config and deployment
 - [x] Code dropbear deployment (mostly manual)
+- [ ] Code basic neovim config and deployment
 - [x] Code dependency installation
 - [ ] Maybe code some would-be-handy scripts
 - [ ] Come up with something more than that
--- setup.sh
+++ setup.sh
@@ -37,10 +37,10 @@ esac
 printf "Installing dependencies listed in dependencies.txt...\n"
 # shellcheck disable=SC2207
 DEPLIST=($(sed -e 's/#.*$//' -e '/^$/d' dependencies.txt))
-apt-get update -y &>/dev/null
-apt-get upgrade -o Dpkg::Options::="--force-confnew" -y &>/dev/null
+apt-get update -y 1>/dev/null
+apt-get upgrade -o Dpkg::Options::="--force-confnew" -y 1>/dev/null
 # shellcheck disable=SC2048 disable=SC2086 # Unquoted is required since pkg cant find a string of packages
-pkg install -y ${DEPLIST[*]} &>/dev/null
+pkg install -y ${DEPLIST[*]} 1>/dev/null
 printf "Installed dependencies\n"
 
 check_dir_exists "$HOME/.termux"
@@ -77,6 +77,8 @@ printf "Adding dots to user home\n"
 cp -f "$source/dots/.zshrc" "$HOME"
 cp -f "$source/dots/.zprofile" "$HOME"
 cp -f "$source/dots/.p10k.zsh" "$HOME"
+echo "termux-wake-lock" > "$HOME/.zlogin"
+echo "termux-wake-unlock" > "$HOME/.zlogout"
 touch "$HOME/.hushlogin"
 
 check_dir_exists "$HOME/.config/powerlevel10k"
@@ -84,10 +86,10 @@ if $exists; then
     printf "your powerlevel10k theme is already installed\n"
 else
     printf "cloning powerlevel10k from github\n"
-    git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" "$HOME/.config/powerlevel10k"
+    git clone --quiet --depth=1 "https://github.com/romkatv/powerlevel10k.git" "$HOME/.config/powerlevel10k"
 fi
 
-printf "LAST STEPS\n"
+printf "\nLAST STEPS\n"
 printf "First of all, add this to your .ssh/config on PC:\n\nHost phone\n\tHostName termux_LAN_address\n\tUser WHOAMI_to_check_username\n\tPort 8022\n"
 printf "\nNext, run:\n\tpasswd\n\t# Set password in termux for initial ssh connection\n"
 printf "\n\tssh-keygen -t rsa -b 4096 -f id_rsa\n\t# Generate ssh keys on PC\n"
