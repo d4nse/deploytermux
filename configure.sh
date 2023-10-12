bc81ebc1ba774771420b8c8cdb450b1487a2456a
d4nse <mordorsocial@disroot.org>
2023-10-12 17:26:10 +0500

    Minor test changes

--- configure.sh
+++ configure.sh
@@ -1,88 +1,85 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 GLOBALSOURCE=$(pwd)
 REQUIRED=(git ncurses-utils zsh termux-services openssh)
 
 #
-#   CHECKS 
+#   CHECKS
 #
 
 echo -e "THIS SCRIPT IS DESTRUCTIVE TO PERSONAL FILES.\nONLY RUN IT ON A CLEAN TERMUX SYSTEM\n."
 read -r -p "You sure you want to proceed? (y/n) " yn
 case $yn in
-    y)  ;;
-    *)
-        echo "Terminating script..."
-        exit 0;;
+y) ;;
+*)
+    echo "Terminating script..."
+    exit 0
+    ;;
 esac
 
 echo -e "Several packages are required for this config to function.\nRequired:\n\t${REQUIRED[*]}"
 read -r -p "Do you want to proceed with their installation now? (y/n) " yn
 case $yn in
-    y)  
-        echo "Updating, might take awhile..."
-        apt-get update -y &>/dev/null
-        apt-get upgrade -o Dpkg::Options::="--force-confnew" -y &>/dev/null
+y)
+    echo "Updating, might take awhile..."
+    apt-get update -y &>/dev/null
+    apt-get upgrade -o Dpkg::Options::="--force-confnew" -y &>/dev/null
 
-        echo "Installing dependencies..."
-        # shellcheck disable=SC2048 disable=SC2086 # Unquoted is required since pkg cant find a string of packages
-        pkg install -y ${REQUIRED[*]};;
-    *)
-        echo "Terminating script..."
-        exit 0;;
+    echo "Installing dependencies..."
+    # shellcheck disable=SC2048 disable=SC2086 # Unquoted is required since pkg cant find a string of packages
+    pkg install -y ${REQUIRED[*]} &>/dev/null
+    ;;
+*)
+    echo "Terminating script..."
+    exit 0
+    ;;
 esac
 
 #
-#	FUNCTIONS	
+#	FUNCTIONS
 #
 
 function configureTermux {
 
-        # Variables
-        local SOURCE DESTINATION
-        DESTINATION="$HOME/.termux"
-        SOURCE="$GLOBALSOURCE/termux"
-        
-        # Check if .termux exists
-        if [[ -d "$DESTINATION" ]];
-        then 
-            # Destroy it and make anew
-            rm -rf "$DESTINATION"
-            mkdir "$DESTINATION"
+    # Variables
+    local DESTINATION SOURCE
+    DESTINATION="$HOME/.termux"
+    SOURCE="$GLOBALSOURCE/termux"
 
-            # Populate it with files from source and reload settings
-            cp -- "$SOURCE/"* "$DESTINATION" && termux-reload-settings
-        fi
+    # Check if .termux exists and delete it
+    echo "Setting up termux properties..."
+    [[ -d "$DESTINATION" ]] && rm -rf "$DESTINATION"
+    mkdir "$DESTINATION" &&
+        cp -- "$SOURCE/"* "$DESTINATION" &&
+        termux-reload-settings # Make sure when cp-ing that * is outside of quotes
 
-        # If storage isnt set up, then set it up
-        if [[ ! -d "$HOME/storage" ]];
-        then
-                echo "Setting up storage..."
-                termux-setup-storage
-        else
-                echo "Storage already set up"
-        fi
+    # If storage isnt set up, then set it up
+    if [[ ! -d "$HOME/storage" ]]; then
+        echo "Setting up storage..."
+        termux-setup-storage
+    else
+        echo "Storage already set up"
+    fi
 }
 
 function configureShell {
 
     # Variables
     local SHELL ZSHRC P10KRC P10KDIR SOURCE
-	SHELL=$(basename "$SHELL")
-	ZSHRC="$HOME/.zshrc"
+    SHELL=$(basename "$SHELL")
+    ZSHRC="$HOME/.zshrc"
     P10KRC="$HOME/.config/p10k.zsh"
     P10KDIR="$HOME/.local/share/powerlevel10k"
     SOURCE="$GLOBALSOURCE/config"
 
     # If ZSH isnt main shell, set it up.
-	if [[ "$SHELL" != "zsh" ]];
-	then
-		echo "Setting up ZSH..."
-		chsh -s zsh
+    if [[ "$SHELL" != "zsh" ]]; then
+        echo "Setting up ZSH..."
+        chsh -s zsh
 
-        # Configure ZSHRC 
-        # shellcheck disable=SC1090 # 
-		[[ ! -f "$ZSHRC" ]] && cp "$SOURCE/zshrc" "$ZSHRC" && zsh && source "$ZSHRC"
+        # Configure ZSHRC
+        # shellcheck disable=SC1090 #
+        [[ ! -f "$ZSHRC" ]] && cp "$SOURCE/zshrc" "$ZSHRC" && zsh && source "$ZSHRC"
 
         # Pre-Configure P10K
         [[ ! -d "$HOME/.config" ]] && mkdir "$HOME/.config"
@@ -90,10 +87,10 @@ function configureShell {
 
         # Install P10K
         [[ ! -d "$P10KDIR" ]] && mkdir -p "$P10KDIR"
-        git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" "$P10KDIR" 
+        git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" "$P10KDIR"
     else
         echo "Seems like ZSH is already set up."
-	fi
+    fi
 }
 
 function configureSSH {
@@ -114,7 +111,7 @@ function closingMessage {
     echo "Set: run passwd to allow ssh connections"
 }
 
-configureTermux
+#configureTermux
 #configureShell
 #configureSSH
 #closingMessage
--- termux/motd.sh
+++ termux/motd.sh
@@ -23,21 +23,11 @@ MESSAGE="${MOTDARR[$randNum]}"
 # Get terminal window info
 COLUMNS=$(tput cols)
 
-# Get ssh info
-DSTATUS=$(sv status sshd | grep -o "down")
-PRIVATEIP=$(ifconfig 2>/dev/null | grep -oE '192\.168\.[0-9]{1,3}\.[0-9]{1,3}' | head -n1)
-
 # Print 
 echo -e "\n\n\n"
 printf "%*s\n" $(((${#MESSAGE}+$COLUMNS)/2)) "$MESSAGE"
 echo -e "\n\n\n"
 printf "%*s\n" $(($COLUMNS-5)) "d4nse"
-if [[ "$DSTATUS" == "down" ]];
-then
-    printf "%*s\n" $(($COLUMNS-5)) "SSHD is down"
-else
-    printf "%*s\n" $(($COLUMNS-5)) "SSHD on $PRIVATEIP"
-fi
 
     
 
